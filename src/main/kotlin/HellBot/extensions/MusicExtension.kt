a830370e21b0dc3dbcc0df3a532e2bfa7ad2ca48
Bas van 't Hazeveld <bas@hazeveld.org>
2025-06-03 18:28:44 +0200

    Refactor MusicExtension to use more descriptive variable names and improve queue management

--- src/main/kotlin/HellBot/extensions/MusicExtension.kt
+++ src/main/kotlin/HellBot/extensions/MusicExtension.kt
@@ -1,25 +1,18 @@
 package HellBot.extensions
 
-import HellBot.extensions.MusicExtension.PlayArguments
 import HellBot.i18n.Translations
 import dev.arbjerg.lavalink.protocol.v4.LoadResult
 import dev.arbjerg.lavalink.protocol.v4.Track
 import dev.kord.common.entity.ButtonStyle
-import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.channel.createMessage
-import dev.kord.core.behavior.edit
 import dev.kord.core.entity.Message
-import dev.kord.core.entity.interaction.followup.EphemeralFollowupMessage
-import dev.kord.core.entity.interaction.followup.FollowupMessage
 import dev.kord.rest.builder.message.embed
 import dev.kordex.core.commands.Arguments
 import dev.kordex.core.commands.converters.impl.attachment
 import dev.kordex.core.commands.converters.impl.string
-import dev.kordex.core.commands.converters.impl.user
 import dev.kordex.core.components.components
 import dev.kordex.core.components.publicButton
 import dev.kordex.core.extensions.Extension
-import dev.kordex.core.extensions.publicSlashCommand
 import dev.kordex.core.extensions.ephemeralSlashCommand
 import dev.kordex.core.i18n.withContext
 import dev.schlaubi.lavakord.audio.TrackEndEvent
@@ -29,7 +22,6 @@ import dev.schlaubi.lavakord.kord.lavakord
 import dev.schlaubi.lavakord.plugins.lavasrc.LavaSrc
 import dev.schlaubi.lavakord.plugins.sponsorblock.Sponsorblock
 import dev.schlaubi.lavakord.rest.loadItem
-import java.util.Queue
 
 class MusicExtension() : Extension() {
 	override val name = "music"
@@ -41,22 +33,22 @@ class MusicExtension() : Extension() {
 		}
 	}
 
-	val Map = LinkedHashMap<ULong, ArrayDeque<Track>>()
-	val MessageMap = LinkedHashMap<ULong, Message>()
+	val linkedHashMap = LinkedHashMap<ULong, ArrayDeque<Track>>()
+	val messageMap = LinkedHashMap<ULong, Message>()
 
 	suspend fun playOrEnqueueSong(guildId: ULong, toPlaytrack: Track, message: Message) {
-		val queue = Map.get(guildId)
+		val queue = linkedHashMap[guildId]
 		if(queue == null) {
-			Map[guildId] = ArrayDeque<Track>().apply { add(toPlaytrack) }
+			linkedHashMap[guildId] = ArrayDeque<Track>().apply { add(toPlaytrack) }
 			val channel = message.getChannel()
 			val newMes = channel.createMessage {
 				content = "Playing song..."
 			}
-			MessageMap[guildId] = newMes
+			messageMap[guildId] = newMes
 
 			val player = lavalink.getLink(guildId).player
 			player.on<TrackStartEvent> {
-				val map = MessageMap[guildId]
+				val map = messageMap[guildId]
 				if (map != null) {
 					val channel = map.getChannel()
 					map.delete()
@@ -111,15 +103,17 @@ class MusicExtension() : Extension() {
 
 								action {
 									lavalink.getLink(guildId).destroy()
+									messageMap[guildId] == null
+									linkedHashMap[guildId] == null
 								}
 							}
 						}
 					}
-					MessageMap[guildId] = newMes
+					messageMap[guildId] = newMes
 				}
 			}
 
-			player.playTrack(Map[guildId]!!.removeFirst())
+			player.playTrack(linkedHashMap[guildId]!!.removeFirst())
 		}else {
 			queue.add(toPlaytrack)
 		}
@@ -140,11 +134,20 @@ class MusicExtension() : Extension() {
 				val player = link.player
 
 				player.on<TrackEndEvent> {
-					if (Map.containsKey(guild!!.id.value)) {
-						val queue = Map[guild!!.id.value]!!
+					if (linkedHashMap.containsKey(guild!!.id.value)) {
+						val queue = linkedHashMap[guild!!.id.value]!!
 						if (queue.isNotEmpty()) {
 							val track = queue.removeFirst()
 							player.playTrack(track)
+						}else {
+							// If the queue is empty, destroy the link
+							link.destroy()
+							val map = messageMap[guild!!.id.value]
+							if (map != null) {
+								map.delete()
+								messageMap[guild!!.id.value] == null
+								linkedHashMap[guild!!.id.value] == null
+							}
 						}
 					}
 				}
@@ -223,11 +226,20 @@ class MusicExtension() : Extension() {
 				val player = link.player
 
 				player.on<TrackEndEvent> {
-					if (Map.containsKey(guild!!.id.value)) {
-						val queue = Map[guild!!.id.value]!!
+					if (linkedHashMap.containsKey(guild!!.id.value)) {
+						val queue = linkedHashMap[guild!!.id.value]!!
 						if (queue.isNotEmpty()) {
 							val track = queue.removeFirst()
 							player.playTrack(track)
+						}else {
+							// If the queue is empty, destroy the link
+							link.destroy()
+							val map = messageMap[guild!!.id.value]
+							if (map != null) {
+								map.delete()
+								messageMap[guild!!.id.value] == null
+								linkedHashMap[guild!!.id.value] == null
+							}
 						}
 					}
 				}
